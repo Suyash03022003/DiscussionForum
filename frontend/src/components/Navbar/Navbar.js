d5fd948a2ccdce10906c5e0e90d6a390537e15e7
rautshreya <126368318+ShreyaR2409@users.noreply.github.com>
2023-10-20 15:33:08 +0530

    Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,23 +1,44 @@
 import { Link } from "react-router-dom";
-import './navbar.module.css';
+import styles from './navbar.module.css';
+import { MdSearch, MdHomeFilled } from "react-icons/md";
+import { BiLogIn} from "react-icons/bi";
 
 const Navbar = () => {
+
   return (
-    <>
-      <nav>
-        <div>
-          <p>Discussion Forum</p>
-        </div>
-        <div className="links">
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/login">Login</Link></p>
-        </div>
-        <div>
-          <p>Discussion Forum</p>
-        </div>
-      </nav>
-    </>
-  )
-};
+    <header className={styles.mainNav}>
+      <div className={styles.navlogo}>
+        <h2>
+          <span>D</span>iscussion
+          <span>F</span>orum
+        </h2>
+      </div>
+
+      <div className={styles.navlinks}>
+        <ul>
+          <li>
+            <Link to="/">
+              <MdHomeFilled color="white" size={45}/>
+            </Link>
+          </li>
+
+          <li>
+            <div className={styles.headerSearchContainer}>
+              <MdSearch color="grey"/>
+              <input type='text'  placeholder='search...' />
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+
+
+      <div className={styles.navlogin}>
+        <BiLogIn color="white" size={45} />
+      </div>
+
+    </header>
+  );
+}
 
 export default Navbar;
